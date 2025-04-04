2ffe2d298f51b560b6db9d51405f7d5a0a424fc9
Mohammed Subhan <subhanm918@gmail.com>
2025-04-04 16:09:06 +0530

    Implement new feature

---
+++ OneDrive - JINN/Streaming Project/config.js
@@ -0,0 +1,12 @@
+// config.js
+const TMDB_API = {
+  KEY: '8623f47fce62cacf80ca14f7d3fa363d', // Get from https://www.themoviedb.org/settings/api
+  BASE_URL: 'https://api.themoviedb.org/3',
+  IMAGE_BASE: 'https://image.tmdb.org/t/p/w500'
+};
+
+// For different media types
+const MEDIA_TYPES = {
+  MOVIE: 'movie',
+  TV: 'tv' // Using TV for dramas
+};
